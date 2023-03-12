54afb429f027aba9224a601d66b0a9a9db8c5f18
vanessrw <64979714+vanessrw@users.noreply.github.com>
2023-03-12 11:18:35 +0700

    GUI

--- Tubes2_Stima/Form1.Designer.cs
+++ Tubes2_Stima/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Tubes2_Stima
+﻿namespace Connect
 {
     partial class Form1
     {
@@ -28,13 +28,296 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_LoadFile = new System.Windows.Forms.Button();
+            this.openFileGraph = new System.Windows.Forms.OpenFileDialog();
+            this.panel_DrawGraph = new System.Windows.Forms.Panel();
+            this.openFileQuery = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_LoadFile
+            // 
+            this.button_LoadFile.AutoSize = true;
+            this.button_LoadFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_LoadFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_LoadFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button_LoadFile.Location = new System.Drawing.Point(75, 279);
+            this.button_LoadFile.Margin = new System.Windows.Forms.Padding(4);
+            this.button_LoadFile.Name = "button_LoadFile";
+            this.button_LoadFile.Size = new System.Drawing.Size(209, 43);
+            this.button_LoadFile.TabIndex = 0;
+            this.button_LoadFile.Text = "e.g \"Maze1.txt\"";
+            this.button_LoadFile.UseVisualStyleBackColor = true;
+            this.button_LoadFile.Click += new System.EventHandler(this.button_LoadFile_Click);
+            // 
+            // openFileGraph
+            // 
+            this.openFileGraph.FileName = "openFileGraph";
+            // 
+            // panel_DrawGraph
+            // 
+            this.panel_DrawGraph.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel_DrawGraph.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel_DrawGraph.Location = new System.Drawing.Point(483, 219);
+            this.panel_DrawGraph.Margin = new System.Windows.Forms.Padding(4);
+            this.panel_DrawGraph.Name = "panel_DrawGraph";
+            this.panel_DrawGraph.Size = new System.Drawing.Size(586, 380);
+            this.panel_DrawGraph.TabIndex = 2;
+            this.panel_DrawGraph.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_DrawGraph_Paint);
+            // 
+            // openFileQuery
+            // 
+            this.openFileQuery.FileName = "openFileQuery";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(338, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(515, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = " Treasure Hunt Solver";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(72, 244);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(217, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Filename            :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(69, 349);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Algorithm            :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton1.Location = new System.Drawing.Point(75, 417);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(93, 35);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "DFS";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton2.Location = new System.Drawing.Point(75, 384);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(91, 35);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "BFS";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(84, 695);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 31);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Routes          :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(84, 737);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(178, 31);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Nodes           :";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(709, 617);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 41);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Search !";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(627, 737);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(278, 31);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Execution Time          :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(75, 28);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(138, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label8.Location = new System.Drawing.Point(70, 176);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(146, 48);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Input";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label9.Location = new System.Drawing.Point(475, 167);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(176, 48);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Output";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label7.Location = new System.Drawing.Point(627, 695);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(274, 31);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Steps                          :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1157, 857);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_LoadFile);
+            this.Controls.Add(this.panel_DrawGraph);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_LoadFile;
+        private System.Windows.Forms.OpenFileDialog openFileGraph;
+        private System.Windows.Forms.Panel panel_DrawGraph;
+        private System.Windows.Forms.OpenFileDialog openFileQuery;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
     }
 }
-
--- Tubes2_Stima/Form1.cs
+++ Tubes2_Stima/Form1.cs
@@ -6,15 +6,265 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
 
-namespace Tubes2_Stima
+namespace Tubes2_stima
 {
+    
     public partial class Form1 : Form
     {
+        
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern System.IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+        private string currentAccount;
+        private string targetAccount;
+
+        Microsoft.Msagl.Drawing.Graph graph; // The graph that MSAGL accepts
+        Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+        // Graph viewer engine
+        private Graph graf;
         public Form1()
         {
             InitializeComponent();
+            button1.Enabled = false;
+        }
+
+        private void button_LoadFile_Click(object sender, EventArgs e)
+        {
+            // Browse Document
+            openFileGraph.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileGraph.InitialDirectory = Directory.GetCurrentDirectory();
+
+            // Show file dialog
+            DialogResult result = openFileGraph.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                // Read input file
+                using (StreamReader bacafile = new StreamReader(openFileGraph.OpenFile()))
+                {
+                    string baca = bacafile.ReadLine();
+                    if (baca == null || baca == "0")
+                    {
+                        MessageBox.Show("File Kosong");
+                    }
+                    else
+                    {
+                        this.graf = new Graph(bacafile);
+                        comboBox1.Items.Clear();
+                        comboBox2.Items.Clear();
+                        foreach (KeyValuePair<string, List<string>> entry1 in graf.getAdjacent())
+                        {
+                            comboBox1.Items.Add(entry1.Key);
+                            comboBox2.Items.Add(entry1.Key);
+                        }
+                            DrawGraph(this.graf);
+                            button1.Enabled = true;
+                            comboBox3.SelectedItem = "Show Graph";
+                    }
+                }
+            }
+        }
+
+        private void DrawGraph(Graph graf)
+        {
+
+            List<Tuple<string, string>> visited = new List<Tuple<string, string>>();
+            graph = new Microsoft.Msagl.Drawing.Graph("graph"); // Initialize new MSAGL graph                
+
+            foreach (KeyValuePair<string, List<string>> entry1 in graf.getAdjacent())
+            {
+                // do something with entry.Value or entry.Key
+                foreach (var entry2 in entry1.Value)
+                {
+                    if (!visited.Contains(Tuple.Create(entry1.Key, entry2)) && !visited.Contains(Tuple.Create(entry2, entry1.Key)))
+                    {
+                        Microsoft.Msagl.Drawing.Edge e;
+                        e = graph.AddEdge(entry1.Key, entry2);
+                        e.Attr.ArrowheadAtTarget = Microsoft.Msagl.Drawing.ArrowStyle.None;
+                        e.Attr.Id = entry1.Key + entry2;
+                    }
+                    visited.Add(Tuple.Create(entry1.Key, entry2));
+                }
+            }
+            drawContainer(graph);
+        }
+        public void drawContainer(Microsoft.Msagl.Drawing.Graph graph)
+        {
+            graph.LayoutAlgorithmSettings = new Microsoft.Msagl.Layout.MDS.MdsLayoutSettings();
+            viewer.CurrentLayoutMethod = Microsoft.Msagl.GraphViewerGdi.LayoutMethod.UseSettingsOfTheGraph;
+            viewer.Graph = graph;
+            // Bind graph to viewer engine
+            viewer.Graph = graph;
+            // Bind viewer engine to the panel
+            panel_DrawGraph.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel_DrawGraph.Controls.Add(viewer);
+            panel_DrawGraph.ResumeLayout();
+        }
+        private void panel_DrawGraph_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Bikin fungsi dfs bfs, trs panggil disini. Ini button submit ceunah wkwk 
+            if (radioButton1.Checked)
+            
+            {
+                
+                if (comboBox3.Text == "Show Graph")
+                {
+                    string x = "DFS1";
+                    DrawGraph(this.graf);
+                    textBox1.Text = x;
+                }
+                else if (comboBox3.Text == "Friend Recommendation")
+                {
+                    Graph.DFS d;
+                    d = new Graph.DFS(this.graf, ref graph, ref panel_DrawGraph, ref viewer);
+                    List<string> recommendation = new List<string>();
+                    List<string> node = new List<string>(d.friendsRecommendation(comboBox1.Text));
+                    string x = "Daftar rekomendasi teman untuk akun " + comboBox1.Text +":\r\n";
+                    x = x + d.friendRecommendationText(comboBox1.Text,node);
+
+                    textBox1.Text = x;
+                }
+                else if (comboBox3.Text == "Explore Friends")
+                {
+                    Graph.DFS d;
+                    d = new Graph.DFS(this.graf, ref graph, ref panel_DrawGraph, ref viewer);
+                    List<string> answer = new List<string>(d.exploreFriends(comboBox1.Text, comboBox2.Text));
+                    string x = "Nama akun : " + comboBox1.Text + " dan " + comboBox2.Text + "\r\n";
+                    x = x + d.exploreFriendsText(answer);
+                    textBox1.Text = x;
+                }
+
+            }
+            if (radioButton2.Checked)
+            {
+                if (comboBox3.Text == "Show Graph")
+                {
+                    string x = "BFS1";
+                    textBox1.Text = x;
+                }
+                else if (comboBox3.Text == "Friend Recommendation")
+                {
+                    Graph.BFS b;
+                    b = new Graph.BFS(this.graf, ref graph, ref panel_DrawGraph, ref viewer);
+                    List<string> recommendation = new List<string>();
+                    List<string> node = new List<string>(b.friendsRecommendation(comboBox1.Text));
+                    string x = "Daftar rekomendasi teman untuk akun " + comboBox1.Text +":\r\n";
+                    x = x + b.friendRecommendationText(comboBox1.Text,node);
+
+                    textBox1.Text = x;
+
+                }
+                else if (comboBox3.Text == "Explore Friends")
+                {
+                    Graph.BFS b;
+                    b = new Graph.BFS(this.graf, ref graph, ref panel_DrawGraph, ref viewer);
+                    List<string> answer = new List<string>(b.exploreFriends(this.currentAccount, this.targetAccount));
+                    string x = "Nama akun : " + comboBox1.Text + " dan " + comboBox2.Text + "\r\n";
+                    x = x + b.exploreFriendsText(answer);
+                    textBox1.Text = x;
+                }
+            }
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.currentAccount = comboBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // kalo mau disable
+            //button2.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, button1.Width, button1.Height, 30, 30));
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.targetAccount = comboBox2.Text;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
