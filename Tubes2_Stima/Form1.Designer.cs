229e8442ab094146636bf62c5c4f4082b1cd1909
vanessrw <64979714+vanessrw@users.noreply.github.com>
2023-03-17 12:35:24 +0700

    minor fix

--- Tubes2_Stima/Form1.Designer.cs
+++ Tubes2_Stima/Form1.Designer.cs
@@ -30,23 +30,16 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button_LoadFile = new System.Windows.Forms.Button();
-            this.openFileGraph = new System.Windows.Forms.OpenFileDialog();
-            this.panel_DrawGraph = new System.Windows.Forms.Panel();
-            this.openFileQuery = new System.Windows.Forms.OpenFileDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button_LoadFile
@@ -62,26 +55,10 @@
             this.button_LoadFile.TabIndex = 0;
             this.button_LoadFile.Text = "e.g \"Maze1.txt\"";
             this.button_LoadFile.UseVisualStyleBackColor = true;
-            this.button_LoadFile.Click += new System.EventHandler(this.button_LoadFile_Click);
-            // 
-            // openFileGraph
-            // 
-            this.openFileGraph.FileName = "openFileGraph";
-            // 
-            // panel_DrawGraph
-            // 
-            this.panel_DrawGraph.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.panel_DrawGraph.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel_DrawGraph.Location = new System.Drawing.Point(483, 219);
-            this.panel_DrawGraph.Margin = new System.Windows.Forms.Padding(4);
-            this.panel_DrawGraph.Name = "panel_DrawGraph";
-            this.panel_DrawGraph.Size = new System.Drawing.Size(586, 380);
-            this.panel_DrawGraph.TabIndex = 2;
-            this.panel_DrawGraph.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_DrawGraph_Paint);
+
             // 
             // openFileQuery
             // 
-            this.openFileQuery.FileName = "openFileQuery";
             // 
             // label1
             // 
@@ -124,38 +101,7 @@
             this.label3.Text = "Algorithm            :";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
-            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.radioButton1.Location = new System.Drawing.Point(75, 417);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(93, 35);
-            this.radioButton1.TabIndex = 6;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "DFS";
-            this.radioButton1.UseVisualStyleBackColor = false;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
-            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.radioButton2.Location = new System.Drawing.Point(75, 384);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(91, 35);
-            this.radioButton2.TabIndex = 7;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "BFS";
-            this.radioButton2.UseVisualStyleBackColor = false;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
-            // 
+
             // label4
             // 
             this.label4.AutoSize = true;
@@ -195,7 +141,6 @@
             this.button1.TabIndex = 12;
             this.button1.Text = "Search !";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label6
             // 
@@ -209,19 +154,6 @@
             this.label6.Size = new System.Drawing.Size(278, 31);
             this.label6.TabIndex = 14;
             this.label6.Text = "Execution Time          :";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(75, 28);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(138, 121);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 18;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // label8
             // 
@@ -275,18 +207,14 @@
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button_LoadFile);
-            this.Controls.Add(this.panel_DrawGraph);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -294,7 +222,6 @@
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -303,7 +230,6 @@
         #endregion
 
         private System.Windows.Forms.Button button_LoadFile;
-        private System.Windows.Forms.OpenFileDialog openFile;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -311,7 +237,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label7;
--- Tubes2_Stima/Form1.cs
+++ Tubes2_Stima/Form1.cs
@@ -14,10 +14,10 @@ using System.Runtime.InteropServices;
 
 namespace Tubes2_stima
 {
-    
+
     public partial class Form1 : Form
     {
-        
+
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         private static extern System.IntPtr CreateRoundRectRgn
         (
@@ -31,43 +31,10 @@ namespace Tubes2_stima
         private string currentAccount;
         private string targetAccount;
 
-
-
-        private void button_LoadFile_Click(object sender, EventArgs e)
+        public Form1()
         {
-            // Browse Document
-            openFileGraph.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-            openFileGraph.InitialDirectory = Directory.GetCurrentDirectory();
-
-            // Show file dialog
-            DialogResult result = openFileGraph.ShowDialog();
-
-            if (result == DialogResult.OK)
-            {
-                // Read input file
-                using (StreamReader bacafile = new StreamReader(openFileGraph.OpenFile()))
-                {
-                    string baca = bacafile.ReadLine();
-                    if (baca == null || baca == "0")
-                    {
-                        MessageBox.Show("File Kosong");
-                    }
-                    else
-                    {
-                        this.graf = new Graph(bacafile);
-                        comboBox1.Items.Clear();
-                        comboBox2.Items.Clear();
-                        foreach (KeyValuePair<string, List<string>> entry1 in graf.getAdjacent())
-                        {
-                            comboBox1.Items.Add(entry1.Key);
-                            comboBox2.Items.Add(entry1.Key);
-                        }
-                            DrawGraph(this.graf);
-                            button1.Enabled = true;
-                            comboBox3.SelectedItem = "Show Graph";
-                    }
-                }
-            }
+            InitializeComponent();
+            button1.Enabled = false;
         }
 
         private void panel_DrawGraph_Paint(object sender, PaintEventArgs e)
@@ -77,7 +44,7 @@ namespace Tubes2_stima
 
         private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void label7_Click(object sender, EventArgs e)
@@ -96,6 +63,16 @@ namespace Tubes2_stima
         }
 
 
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
@@ -106,10 +83,6 @@ namespace Tubes2_stima
 
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            this.currentAccount = comboBox1.Text;
-        }
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -120,11 +93,6 @@ namespace Tubes2_stima
         private void Form1_Load(object sender, EventArgs e)
         {
             button1.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, button1.Width, button1.Height, 30, 30));
-        }
-
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            this.targetAccount = comboBox2.Text;
         }
 
         private void textBox1_TextChanged_1(object sender, EventArgs e)
