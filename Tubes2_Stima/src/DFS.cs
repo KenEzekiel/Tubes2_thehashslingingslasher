6ef31d47695afd6119082d30794a5e1871f8ba77
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-03-21 18:14:53 +0700

    fix formatting dfs

--- Tubes2_Stima/src/DFS.cs
+++ Tubes2_Stima/src/DFS.cs
@@ -22,41 +22,50 @@ namespace Tubes2_Stima.src
             this.numOfTreasure = numOfTreasure;
         }
 
-        public override void insertNode(Block n, char a) {
+        public override void insertNode(Block n, char a)
+        {
             (char move, Block node) temp = (a, n);
             this.NodeMoves.Push(temp);
         }
 
-        public override void insertChild(Block n, char lastMove, ref bool notDeadend) {
+        public override void insertChild(Block n, char lastMove, ref bool notDeadend)
+        {
             // Priority : L U R D
-            if (n.hasL && !(lastMove == 'R')) {
+            if (n.hasL && !(lastMove == 'R'))
+            {
                 insertNode(n.getL(), 'L');
                 notDeadend = true;
             }
-            if (n.hasU && !(lastMove == 'D')) {
+            if (n.hasU && !(lastMove == 'D'))
+            {
                 insertNode(n.getU(), 'U');
                 notDeadend = true;
             }
-            if (n.hasR && !(lastMove == 'L')) {
+            if (n.hasR && !(lastMove == 'L'))
+            {
                 insertNode(n.getR(), 'R');
                 notDeadend = true;
             }
-            if (n.hasD && !(lastMove == 'U')) {
+            if (n.hasD && !(lastMove == 'U'))
+            {
                 insertNode(n.getD(), 'D');
                 notDeadend = true;
             }
         }
 
-        public (char move, Block node) getChild() {
+        public (char move, Block node) getChild()
+        {
             return this.NodeMoves.Pop();
         }
-        public override string startSearch(Block n, bool TSP) {
-            // DFS with IDS, initial depth limit = 5
+
+        public override string startSearch(Block n, bool TSP)
+        {
             string moves = "";
             char lastMove = 'S';
             moves = Search(n, ref lastMove, moves, TSP);
             // tinggal return moves
-            if (Treasure.getTreasureCount() != Treasure.getTreasureTaken()) {
+            if (Treasure.getTreasureCount() != Treasure.getTreasureTaken())
+            {
                 Console.WriteLine("No path found");
                 return "";
             }
@@ -64,113 +73,141 @@ namespace Tubes2_Stima.src
             return moves;
         }
 
-        public void reverseMove(ref string currentMoves, char lastMove) {
-                if (lastMove == 'L') {
-                    currentMoves += "R";
-                }
-                if (lastMove == 'U') {
-                    currentMoves += "D";
-                }
-                if (lastMove == 'R') {
-                    currentMoves += "L";
-                }
-                if (lastMove == 'D') {
-                    currentMoves += "U";
-                }
+        public void reverseMove(ref string currentMoves, char lastMove)
+        {
+            if (lastMove == 'L')
+            {
+                currentMoves += "R";
+            }
+            if (lastMove == 'U')
+            {
+                currentMoves += "D";
+            }
+            if (lastMove == 'R')
+            {
+                currentMoves += "L";
+            }
+            if (lastMove == 'D')
+            {
+                currentMoves += "U";
+            }
         }
 
-        public string Search(Block node, ref char lastMove, string moves, bool TSP) {
-
+        public string Search(Block node, ref char lastMove, string moves, bool TSP)
+        {
             string currentMoves = moves;
-            
 
             if (Treasure.getTreasureCount() == Treasure.getTreasureTaken())
             {
                 moves = currentMoves;
                 return currentMoves;
-            } else {
+            }
+            else
+            {
                 node.step();
                 // di step nya treasure, tambahin num of treasure++
                 // terus nanti get num of gotten treasurenya, bandingin sama total treasure
                 // tambahin currentMove. terus kasih ke block ID
                 currentMoves += lastMove;
                 this.BlockIDMovesMapping[node.getID()] = currentMoves;
-            
-            
-            // cari child nya yang bukan parentnya
-            bool notDeadend = false;
-            insertChild(node, lastMove,ref notDeadend);
 
-            // kalo TSP nanti pas treasure countnya dah sesuai, tambahin start sebagai treasure terakhir (beda module aja)
-            // Udah di taken, tinggal ditambahin treasure count, kalau misal udah sama yauda stop, kaloga mulai balik ke start kalo TSP
+                // cari child nya yang bukan parentnya
+                bool notDeadend = false;
+                insertChild(node, lastMove, ref notDeadend);
 
-            // if (treasure udah keambil semua) { kalo TSP, backtrack (reverse moves nya ato cari Start sbg Treasure terakhir, boleh, jangan lupa aja block ID nya di reset semua, kalau ga yauda beresin aja langsung return)}
+                // kalo TSP nanti pas treasure countnya dah sesuai, tambahin start sebagai treasure terakhir (beda module aja)
+                // Udah di taken, tinggal ditambahin treasure count, kalau misal udah sama yauda stop, kaloga mulai balik ke start kalo TSP
 
-            if (notDeadend) {
-                
-                // lanjut
-                (char move, Block node) Child;
-                char nextMove;
-                Block nextBlock;
-                if (lastMove == 'S') {
-                    
-                    for (int i = 0; i < node.getNumOfChild(); i++) {
-                        // Search untuk semua child yang dimiliki oleh node yang sedang diinjak
-                        Child = getChild();
-                        nextMove = Child.move;
-                        nextBlock = Child.node;
-                        if (BlockIDMovesMapping.ContainsKey(nextBlock.ID) && currentMoves.StartsWith(BlockIDMovesMapping[nextBlock.ID])) {
-                            // Block yang akan dikunjungi sudah pernah dikunjungi oleh track yang sama
-                            // return currentMoves;
-                        } else {
-                            currentMoves = Search(nextBlock, ref nextMove, currentMoves, TSP);
+                // if (treasure udah keambil semua) { kalo TSP, backtrack (reverse moves nya ato cari Start sbg Treasure terakhir, boleh, jangan lupa aja block ID nya di reset semua, kalau ga yauda beresin aja langsung return)}
+
+                if (notDeadend)
+                {
+                    // lanjut
+                    (char move, Block node) Child;
+                    char nextMove;
+                    Block nextBlock;
+                    if (lastMove == 'S')
+                    {
+                        for (int i = 0; i < node.getNumOfChild(); i++)
+                        {
+                            // Search untuk semua child yang dimiliki oleh node yang sedang diinjak
+                            Child = getChild();
+                            nextMove = Child.move;
+                            nextBlock = Child.node;
+                            if (
+                                BlockIDMovesMapping.ContainsKey(nextBlock.ID)
+                                && currentMoves.StartsWith(BlockIDMovesMapping[nextBlock.ID])
+                            )
+                            {
+                                // Block yang akan dikunjungi sudah pernah dikunjungi oleh track yang sama
+                                // return currentMoves;
+                            }
+                            else
+                            {
+                                currentMoves = Search(nextBlock, ref nextMove, currentMoves, TSP);
+                            }
+                        }
+
+                        if (TSP || !(Treasure.getTreasureCount() == Treasure.getTreasureTaken()))
+                        {
+                            reverseMove(ref currentMoves, lastMove);
                         }
                     }
-                
-                if (TSP || !(Treasure.getTreasureCount() == Treasure.getTreasureTaken())) {
-                    reverseMove(ref currentMoves, lastMove);
-                }
-                } else {
-                    for (int i = 0; i < node.getNumOfChild() - 1; i++) {
-                        // Search untuk semua child yang dimiliki oleh node yang sedang diinjak
-                        Child = getChild();
-                        nextMove = Child.move;
-                        nextBlock = Child.node;
-                        if (BlockIDMovesMapping.ContainsKey(nextBlock.ID) && currentMoves.StartsWith(BlockIDMovesMapping[nextBlock.ID])) {
-                            // Block yang akan dikunjungi sudah pernah dikunjungi oleh track yang sama
-                            // return currentMoves;
-                        } else {
-                            currentMoves = Search(nextBlock, ref nextMove, currentMoves, TSP);
-                            
-                        }  
+                    else
+                    {
+                        for (int i = 0; i < node.getNumOfChild() - 1; i++)
+                        {
+                            // Search untuk semua child yang dimiliki oleh node yang sedang diinjak
+                            Child = getChild();
+                            nextMove = Child.move;
+                            nextBlock = Child.node;
+                            if (
+                                BlockIDMovesMapping.ContainsKey(nextBlock.ID)
+                                && currentMoves.StartsWith(BlockIDMovesMapping[nextBlock.ID])
+                            )
+                            {
+                                // Block yang akan dikunjungi sudah pernah dikunjungi oleh track yang sama
+                                // return currentMoves;
+                            }
+                            else
+                            {
+                                currentMoves = Search(nextBlock, ref nextMove, currentMoves, TSP);
+                            }
+                        }
+                        if (TSP || !(Treasure.getTreasureCount() == Treasure.getTreasureTaken()))
+                        {
+                            reverseMove(ref currentMoves, lastMove);
+                        }
                     }
-                    if (TSP || !(Treasure.getTreasureCount() == Treasure.getTreasureTaken())) {
+
+                    return currentMoves;
+                }
+                else
+                {
+                    // deadend, backtrack
+                    if (TSP || !(Treasure.getTreasureCount() == Treasure.getTreasureTaken()))
+                    {
                         reverseMove(ref currentMoves, lastMove);
+                        if (lastMove == 'L')
+                        {
+                            lastMove = 'R';
+                        }
+                        else if (lastMove == 'U')
+                        {
+                            lastMove = 'D';
+                        }
+                        else if (lastMove == 'R')
+                        {
+                            lastMove = 'L';
+                        }
+                        else if (lastMove == 'D')
+                        {
+                            lastMove = 'U';
+                        }
                     }
+
+                    return currentMoves;
                 }
-                
-                return currentMoves;
-                
-            } else {
-                // deadend, backtrack
-                if (TSP || !(Treasure.getTreasureCount() == Treasure.getTreasureTaken())) {
-                    reverseMove(ref currentMoves, lastMove);
-                    if (lastMove == 'L') {
-                        lastMove = 'R';
-                    }
-                    else if (lastMove == 'U') {
-                        lastMove = 'D';
-                    }
-                    else if (lastMove == 'R') {
-                        lastMove = 'L';
-                    }
-                    else if (lastMove == 'D') {
-                        lastMove = 'U';
-                    }
-                }
-                
-                return currentMoves;
-            }
             }
         }
     }
